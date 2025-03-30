1ff0aebfe4039c779baba0e8a6127cc2a1b02d5a
Samuel <samuel7123@gmail.com>
2025-03-30 17:14:21 -0300

    Registro de usuario, login y recuperación de contraseña

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,9 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import LoginForm  from "./pages/LoginForm.jsx";
+import RegisterForm  from "./pages/RegisterForm.jsx";
+import ForgotPasswordForm from "./pages/ForgotPasswordForm.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -28,6 +31,9 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<LoginForm />} path="/login"/>
+                        <Route element={<RegisterForm />} path="/register"/>
+                        <Route element={<ForgotPasswordForm />} path="/login/forgotpass"/>
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
