bb7cf8bf044b245d0e22b266f5dc299e65008387
Damián Ramírez <146871059+damianconpan@users.noreply.github.com>
2025-04-04 23:14:20 +0000

    avance home y toplikes

--- src/front/js/component/TopLikes.js
+++ src/front/js/component/TopLikes.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { FaHeart } from "react-icons/fa"; // Ícono de corazón
-import tatuaje from "../../img/tatuaje.webp";
+import tatuaje3 from "../../img/tatuaje3.png"; // Imagen 1
+import tatuaje4 from "../../img/tatuaje4.png"; // Imagen 2
+import tatuaje5 from "../../img/tatuaje5.jpg"; // Imagen 3
 
 export const TopLikes = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -10,6 +12,9 @@ export const TopLikes = () => {
     2: { liked: false, likes: 3500 },  // Imagen 2 con 3500 likes iniciales
     3: { liked: false, likes: 4323 },  // Imagen 3 con 4323 likes iniciales
   });
+
+  // Array de imágenes para cada card
+  const images = [tatuaje3, tatuaje4, tatuaje5];
 
   const handleImageClick = (index) => {
     setSelectedImage(index);
@@ -34,15 +39,14 @@ export const TopLikes = () => {
           {[1, 2, 3].map((index) => (
             <div key={index} className="card" style={{ width: "18rem" }}>
               <img
-                src={tatuaje}
+                src={images[index - 1]} // Asignamos la imagen correspondiente
                 className="card-img-top"
-                alt="Tatuaje"
+                alt={`Tatuaje ${index}`}
                 onClick={() => handleImageClick(index)}
                 style={{ cursor: "pointer" }}
               />
               <div className="card-body">
                 <p className="card-text">Descripción del tatuaje #{index}</p>
-                {/* Botón de Like en la card */}
                 <button
                   className={`btn ${likesState[index]?.liked ? "btn-danger" : "btn-outline-danger"}`}
                   onClick={() => toggleLike(index)}
@@ -55,12 +59,10 @@ export const TopLikes = () => {
         </div>
       </div>
 
-      {/* Modal de Bootstrap */}
       {showModal && (
         <div className="modal fade show d-block" tabIndex="-1" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
           <div className="modal-dialog modal-lg">
             <div className="modal-content position-relative">
-              {/* Botón de cierre en la esquina superior derecha DEL MODAL */}
               <button
                 type="button"
                 className="btn-close"
@@ -78,12 +80,11 @@ export const TopLikes = () => {
               </div>
 
               <div className="modal-body d-flex">
-                <img src={tatuaje} alt="Tatuaje" className="img-fluid w-50" />
+                <img src={images[selectedImage - 1]} alt={`Tatuaje ${selectedImage}`} className="img-fluid w-50" />
                 <div className="ms-3">
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum.
                   </p>
-                  {/* Botón de Like dentro del modal */}
                   <button
                     className={`btn ${likesState[selectedImage]?.liked ? "btn-danger" : "btn-outline-danger"}`}
                     onClick={() => toggleLike(selectedImage)}
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,10 +10,8 @@ import { Single } from "./pages/single";
 import LoginForm  from "./pages/LoginForm.jsx";
 import RegisterForm  from "./pages/RegisterForm.jsx";
 import ForgotPasswordForm from "./pages/ForgotPasswordForm.jsx";
-
 import { Category } from "./pages/Category.jsx";
 import TattooerProfile from "./pages/TattooerProfile.jsx";
-import EditTattooerProfile from "./pages/EditTattooerProfile.jsx";
 import injectContext from "./store/appContext";
 
 
@@ -44,7 +42,6 @@ const Layout = () => {
 
                         <Route element={<Category />} path="/category/:categoryId" />
                         <Route element={<TattooerProfile />} path="/tattooer/:id" />
-                        <Route element={<EditTattooerProfile />} path="/edit-profile" />
 
 
                         <Route element={<h1>Not found!</h1>} />
