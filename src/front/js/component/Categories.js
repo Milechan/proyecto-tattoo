8503bb6bbbe7b494f8db5dd2d5f451d2cac554ed
milena <milena.concha.m@gmail.com>
2025-04-11 17:42:06 -0400

    se arreglan links de categorias

--- src/front/js/component/Categories.js
+++ src/front/js/component/Categories.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Categories = () => {
+
     return (
         <div className="text-center mt-5">
-			<div className="container-flex">
-				<h1>categorias</h1>
-				<div className="d-flex justify-content-center">
-                    <button type="button" className="btn burdeo mx-4">Neotradicional</button>
-                    <button type="button" className="btn burdeo mx-4">Geecks</button>
-                    <button type="button" className="btn burdeo mx-4">Minimalista</button>
-                    <button type="button" className="btn burdeo mx-4">Black-Out</button>
-                    <button type="button" className="btn burdeo mx-4">Realismo</button>
+            <div className="container-flex">
+                <h1>categorias</h1>
+                <div className="d-flex justify-content-center">
+                    <Link to="/category/Neotradicional" className="btn burdeo mx-4">Neotradicional</Link>
+                    <Link to="/category/Geeks" className="btn burdeo mx-4">Geeks</Link>
+                    <Link to="/category/Minimalista" className="btn burdeo mx-4">Minimalista</Link>
+                    <Link to="/category/Black-Out" className="btn burdeo mx-4">Black-Out</Link>
+                    <Link to="/category/Realismo" className="btn burdeo mx-4">Realismo</Link>
                 </div>
 
-			</div>
+            </div>
         </div>
     )
 };
