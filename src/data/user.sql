ff4af656d6bf7ee07c82038662fc0a35d78bb6b9
milena <milena.concha.m@gmail.com>
2025-04-13 14:22:39 -0400

    agrego info a query sql de geek

--- src/data/user.sql
+++ src/data/user.sql
@@ -5,8 +5,8 @@ INSERT INTO user (name,username,password,email,notification_enabled,user_type_id
 ("Asmodeus","asmodeus_tattoo","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","asmodeus.tattoo@gmail.com",true,2,1),
 ("Monse Santelices","ateskun_","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","ateskun@gmail.com",true,2,1),
 --geeks
-("Conejo Galactico","conejogalactico","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","conejogalactico@gmail.com",true,2,2),
-("Camila Neira","deadmermaid.tattoo","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","deadmermaid.tattoo@gmail.com",true,2,2), 
+("dearmermaid","deadmermaid.tattoo","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","deadmermaid@gmail.com",true,2,2),
+("dr.etherlnk","dr.etherink","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","dr.etherink@gmail.com",true,2,2), 
 ("Lissalme","lissssalme","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","lissssalme@gmail.com",true,2,2),
 ("Alejandra","aleinnk","scrypt:32768:8:1$YIds4JWIXsmbVHTD$a7400517853410c14e8227988a778cb46044ac0d8daafc048ccf58ec2fec6d03ffa5cecb699cbffb3e25b056e94cd730480f8ab0f329a904b9ad127b10468d81","aleinnk@gmail.com",true,2,2),
 --minimalista
