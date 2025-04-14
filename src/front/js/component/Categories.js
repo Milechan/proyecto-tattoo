e2c78ef123f343a4d72be3d3e5fbad676e491de0
damian <damian.ramirez@ug.uchile.cl>
2025-04-13 21:09:46 -0400

    cambios en el navbar, botones categorias, toplikes y toptattooers

--- src/front/js/component/Categories.js
+++ src/front/js/component/Categories.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import categories from "../../styles/categories.css";
+import "../../styles/categories.css";
 
 export const Categories = () => {
+    const tattooCategories = [
+        "Neotradicional",
+        "Geeks",
+        "Minimalista",
+        "Black-Out",
+        "Realismo",
+    ];
+
     return (
-        <div className="text-center mt-5">
-            <div className="container-flex">
-                <div className="d-flex flex-wrap justify-content-center mt-4 gap-4">
-                    <Link to="/category/Neotradicional" className="btn burdeo px-4 py-3 fs-5 bradius category-button">
-                        Neotradicional
+        <section className="categories-section">
+            <h2 className="categories-title">Explora por Estilo</h2>
+            <div className="categories-container">
+                {tattooCategories.map((category) => (
+                    <Link
+                        key={category}
+                        to={`/category/${category}`}
+                        className="category-button"
+                    >
+                        {category}
                     </Link>
-                    <Link to="/category/Geeks" className="btn burdeo px-4 py-3 fs-5 bradius category-button">
-                        Geeks
-                    </Link>
-                    <Link to="/category/Minimalista" className="btn burdeo px-4 py-3 fs-5 bradius category-button">
-                        Minimalista
-                    </Link>
-                    <Link to="/category/Black-Out" className="btn burdeo px-4 py-3 fs-5 bradius category-button">
-                        Black-Out
-                    </Link>
-                    <Link to="/category/Realismo" className="btn burdeo px-4 py-3 fs-5 bradius category-button">
-                        Realismo
-                    </Link>
-                </div>
+                ))}
             </div>
-        </div>
+        </section>
     );
 };
